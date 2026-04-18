c3e8c9caa5938d56b359d7eea306dbc4a6812c90
Jhon Rowel Abines <jraf2003@gmail.com>
2026-04-18 11:29:15 +0800

    add terms and privacy

--- script.js
+++
@@ -1,8 +0,0 @@
-function showSection(id) {
-  document
-    .querySelectorAll("section")
-    .forEach((s) => s.classList.remove("active"));
-  document.getElementById(id).classList.add("active");
-  window.scrollTo({ top: 0, behavior: "smooth" });
-  return false;
-}
